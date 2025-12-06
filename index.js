6123425e97c0b0b409b775b07a624b1f00e14891
cnfc29 <cnfc629@gmail.com>
2025-12-06 12:51:28 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const a = 5;
+
+if (a == 5) {
+  console.log("a is 5");
+} else {
+  console.log("a is not 5");
+}
---
+++ script.js
@@ -0,0 +1,7 @@
+let b = "Hello";
+
+if (typeof b == "string") {
+  console.log("b is a string");
+} else {
+  console.log("b is not a string");
+}
