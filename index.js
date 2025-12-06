c6bd26e1801b678691984ae90215fd5a84337e8f
cnfc29 <cnfc629@gmail.com>
2025-12-06 15:34:07 +0300

    4 вместо 5

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const a = 5;
+const a = 4;
 
-if (a == 5) {
-  console.log("a is 5");
+if (a == 4) {
+  console.log("a is 4");
 } else {
-  console.log("a is not 5");
+  console.log("a is not 4");
 }
 
 console.log("Hello from index.js");
 console.log('Bay bay');
+console.log('4')
